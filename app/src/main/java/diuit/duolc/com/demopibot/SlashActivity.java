c9cded6ada4bdadf25a4ff36a22dd8ae3beb1786
zxcvbnius <zxcvbnius@gmail.com>
2016-04-18 22:19:40 +0800

    first commit

---
+++ app/src/main/java/diuit/duolc/com/demopibot/SlashActivity.java
@@ -0,0 +1,15 @@
+package diuit.duolc.com.demopibot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlashActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slash);
+    }
+}
